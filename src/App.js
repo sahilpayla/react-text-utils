58781fe2e7f2d0afc5bf6bb3487da670afcd2994
Shraddhanand <1shraddhanand@gmail.com>
2022-12-24 22:02:59 +0530

    add css

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="navbar">
+        <div className='logo'>
+          paste logo here
+        </div>
+        <div className='company-name'>
+          <h1>Planning Paras For Family</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
